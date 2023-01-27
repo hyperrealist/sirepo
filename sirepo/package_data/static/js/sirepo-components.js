30145728a6ab4b7b5cdb934f3fdc56c2023af7aa
mkeilman <mkeilman@radiasoft.net>
2023-01-27 08:54:57 -0700

    #3735 - panel buttons

--- sirepo/package_data/static/js/sirepo-components.js
+++ sirepo/package_data/static/js/sirepo-components.js
@@ -2098,6 +2098,7 @@ SIREPO.app.directive('panelHeading', function(appState, frameCache, panelState, 
         },
         template: `
             <div data-simple-heading="{{ panelHeading }}" data-model-key="modelKey">
+            <div class="model-panel-heading-buttons"></div>
               <a href data-ng-show="hasEditor && ! utilities.isFullscreen()" data-ng-click="showEditor()" title="Edit"><span class="sr-panel-heading glyphicon glyphicon-pencil"></span></a>
               ${SIREPO.appPanelHeadingButtons || ''}
               <div data-ng-if="isReport" data-ng-show="hasData() && ! utilities.isFullscreen()" class="dropdown" style="display: inline-block">
