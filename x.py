2ae0886ba7e939a63ee761a0db855668cc0dda18
gurhar1133 <ghns1133@gmail.com>
2023-04-05 21:59:49 +0000

    Fix #5725: WIP

--- x.py
+++ x.py
@@ -9,11 +9,14 @@ from pykern.pkdebug import pkdp
 
 _EXCLUDE_FILES = re.compile(
     # TODO (gurhar1133): different exclude for replacement?
-    f".*{pkunit.WORK_DIR_SUFFIX}/" + r".*(_console\.py)|^venv/" + r"|^run/" + r"|__pycache__/|.git|.cache|node_modules|react/public|.png|.jpg|.woff|.eot|.ttf|.tif|.gif|.ico|.h5m|.sdds|.zip|.db|.csv|.h5|.bun|.stl|.log"
+    f".*{pkunit.WORK_DIR_SUFFIX}/"
+    + r".*(_console\.py)|^venv/"
+    + r"|^run/"
+    + r"|__pycache__/ "
+    + r"|.git|.cache|node_modules|react/public|.png|.jpg|.woff|.eot|.ttf|.tif|.gif|.ico|.h5m|.sdds|.zip|.db|.csv|.h5|.bun|.stl|.log"
 )
 
-
-_INCLUDE_FILES = "?"
+_INCLUDE_FILES = ""
 
 class _Renamer:
     def __init__(self, old_app_name, new_app_name):
@@ -41,18 +44,12 @@ class _Renamer:
                 str(file_path),
                 d + "/" + b.replace(self.old_app_name, self.new_app_name)
             )
-            print(
-                f"FILE renamed {str(file_path)} \nto {str(file_path).replace(self.old_app_name, self.new_app_name)}"
-            )
 
     def _rename_dir(self, file_path):
         if self.old_app_name in file_path.dirname:
             d = str(file_path.dirname)
             if os.path.exists(d):
                 os.rename(d, d.replace(self.old_app_name, self.new_app_name))
-                print(f"DIR renamed {d} \nto {d.replace(self.old_app_name, self.new_app_name)}")
-            else:
-                print(d, "does not exist (anymore)")
 
     def _rename_references(self):
         self._replace_references()
@@ -67,9 +64,9 @@ class _Renamer:
         for f in pkio.walk_tree("./"):
             if self._exlude(f):
                 continue
-            print("attempting replacement on", f.dirname + "/" + f.basename)
+            # print("attempting replacement on", f.dirname + "/" + f.basename)
             with pkio.open_text(f) as t:
-                # TODO (gurhar1133): need to handle camel case etc
+                # TODO (gurhar1133): need to handle camel case etc?
                 t = t.read()
                 self._replace(f, t, self.old_app_name, self.new_app_name)
                 self._replace(f, t, self.old_app_name.title(), self.new_app_name.title())
@@ -77,8 +74,6 @@ class _Renamer:
 
     def _replace(self, file, text, reference, replacement):
         if re.search(re.compile(reference), text):
-            # TODO (gurhar1133): new_app_name needs to match the case of
-            # old_app_name
             pkio.write_text(
                 file,
                 text.replace(reference, replacement)
@@ -86,7 +81,6 @@ class _Renamer:
 
 
     def _raise_for_references(self):
-
         output = subprocess.check_output(
             [
                 "grep",
@@ -97,34 +91,15 @@ class _Renamer:
                 "--exclude-dir='run'",
                 "--exclude='./x.py'",
                 "--exclude-dir='sirepo.egg-info'",
-                # f"--exclude-dir={self.exclude_files}",
                 f"{self.old_app_name}",
             ]
         ).decode('utf-8').split('\n')[:-1]
-        # p = subprocess.run(
-        #     [
-        #         "grep",
-        #         "-r",
-        #         "-i",
-        #         "-I",
-        #         "--exclude-dir='.pytest_cache'",
-        #         "--exclude-dir='run'",
-        #         "--exclude='./x.py'",
-        #         "--exclude-dir='sirepo.egg-info'",
-        #         f"{self.old_app_name}",
-        #     ],
-        #     stdout=subprocess.PIPE,
-        #     stderr=subprocess.PIPE,
-        # )
-        # print("len", len(p.stdout))
         r = []
         for line in output:
             if not re.search(self.exclude_files, line):
-                r.append(line.split(":")[0])
+                r.append(line)
         if len(r) > 0:
-            for line in r:
-                print(line)
-            raise AssertionError(f"{len(r)} REFERENCES TO {self.old_app_name} FOUND")
+            raise AssertionError(f"{"\n".join(r)}\n{len(r)} REFERENCES TO {self.old_app_name} FOUND")
         print(f"No references to old_app_name={self.old_app_name} found")
 
     def rename(self):
@@ -132,6 +107,10 @@ class _Renamer:
         self._rename_paths()
         self._rename_references()
 
-# _Renamer("mybetterappname", "mybetterapp").rename()
-a = sys.argv
-_Renamer(a[1], a[2]).rename()
+
+def main():
+    a = sys.argv
+    _Renamer(a[1], a[2]).rename()
+
+if __name__ == "__main__":
+   main()
