2bb14c683b49b0fd3ae2410d54c910d6c0d90ae9
gurhar1133 <ghns1133@gmail.com>
2023-04-05 19:10:03 +0000

    Fix #5725: wip

--- x.py
+++ x.py
@@ -9,7 +9,7 @@ from pykern.pkdebug import pkdp
 
 _EXCLUDE_FILES = re.compile(
     # TODO (gurhar1133): different exclude for replacement?
-    r".*(_console\.py)|^venv/" + r"|^run/" + r"|__pycache__/|.git|.cache|node_modules|react/public|.png|.jpg|.woff|.eot|.ttf|.tif|.gif|.ico|.h5m|.sdds|.zip|.db"
+    r".*(_console\.py)|^venv/" + r"|^run/" + r"|__pycache__/|.git|.cache|node_modules|react/public|.png|.jpg|.woff|.eot|.ttf|.tif|.gif|.ico|.h5m|.sdds|.zip|.db|.csv|.h5"
 )
 
 
