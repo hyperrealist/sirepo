51c4d8db8c7f6f4c181e21877d21d28342e91b60
gurhar1133 <ghns1133@gmail.com>
2023-04-05 14:08:46 +0000

    Fix #5725: renamer class

--- x.py
+++ x.py
@@ -9,48 +9,53 @@ _EXCLUDE_FILES = re.compile(
     r".*(_console\.py)|^tests/|^venv/" + r"|^run/" + r"|__pycache__/"
 )
 
-def rename_paths(old_app_name, new_app_name):
-    # rename base and dirnames
-    def _iterate(rename_function):
+class _Renamer:
+    def __init__(self, old_app_name, new_app_name):
+        self.old_app_name = old_app_name
+        self.new_app_name = new_app_name
+        self.exclude_files = _EXCLUDE_FILES
+
+    def _iterate(self, rename_function):
         for f in pkio.walk_tree("./"):
             if re.search(_EXCLUDE_FILES, pkio.py_path().bestrelpath(f)):
                 continue
-            rename_function(f, old_app_name, new_app_name)
+            rename_function(f)
 
-    _iterate(_rename_file)
-    _iterate(_rename_dir)
+    def _rename_paths(self):
+        # rename base and dirnames
+        self._iterate(self._rename_file)
+        self._iterate(self._rename_dir)
 
-def _rename_file(file_path, old_app_name, new_app_name):
-    if old_app_name in file_path.basename:
-        os.rename(str(file_path), str(file_path.dirname) + "/" + str(file_path.basename).replace(old_app_name, new_app_name))
-        print(f"FILE renamed {str(file_path)} \nto {str(file_path).replace(old_app_name, new_app_name)}")
-    return
+    def _rename_file(self, file_path):
+        if self.old_app_name in file_path.basename:
+            d = str(file_path.dirname)
+            b = str(file_path.basename)
+            os.rename(
+                str(file_path),
+                d + "/" + b.replace(self.old_app_name, self.new_app_name)
+            )
+            print(
+                f"FILE renamed {str(file_path)} \nto {str(file_path).replace(self.old_app_name, self.new_app_name)}"
+            )
 
-def _rename_dir(file_path, old_app_name, new_app_name):
-    if old_app_name in file_path.dirname:
-        d = str(file_path.dirname)
-        if os.path.exists(d):
-            os.rename(d, d.replace(old_app_name, new_app_name))
-            print(f"DIR renamed {d} \nto {d.replace(old_app_name, new_app_name)}")
-        else:
-            print(d, "does not exist (anymore)")
-    return
+    def _rename_dir(self, file_path):
+        if self.old_app_name in file_path.dirname:
+            d = str(file_path.dirname)
+            if os.path.exists(d):
+                os.rename(d, d.replace(self.old_app_name, self.new_app_name))
+                print(f"DIR renamed {d} \nto {d.replace(self.old_app_name, self.new_app_name)}")
+            else:
+                print(d, "does not exist (anymore)")
 
+    def _rename_references(self):
+        # replace all instances of app_name in
+        # source code. Needs to include different cases
+        # ie myapp, my_app, MyApp or whatever
+        pass
 
-def rename_references(app_name):
-    # replace all instances of app_name in
-    # source code. Needs to include different cases
-    # ie myapp, my_app, MyApp or whatever
-    pass
+    def rename(self):
+        print(f"renaming {self.old_app_name} to {self.new_app_name}")
+        self._rename_paths()
+        self._rename_references()
 
-def iterate_files(app_name):
-    for f in pkio.walk_tree("./"):
-        if app_name in f.basename:
-            print("base hit", f.basename, f.dirname)
-    print("\n\n\n\n")
-    for f in pkio.walk_tree("./"):
-        if app_name in f.dirname:
-            print("dir hit", f)
-
-print("iteration with exclusion:")
-rename_paths("myapp", "mybetterapp")
+_Renamer("myapp", "mybetterapp").rename()
