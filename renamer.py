fea3a2d103286b8bb09922bd85a8a010e2b99733
gurhar1133 <ghns1133@gmail.com>
2023-04-06 22:47:19 +0000

    Fix #5725: WIP

--- renamer.py
+++ renamer.py
@@ -14,7 +14,8 @@ _EXCLUDE_FILES = re.compile(
     + r".*(_console\.py)|^venv/"
     + r"|^run/"
     + r"|__pycache__/ "
-    + r"|.git|.cache|node_modules|react/public|.png|.jpg|.woff|.eot|.ttf|.tif|.gif|.ico|.h5m|*/.sdds|.zip|.db|.csv|.h5|.bun|.stl|.log|.paramOpt"
+    + r"|.git|.cache|node_modules|react/public|.png|.jpg|.woff|.eot|.ttf|.tif|.gif|.ico|.h5m|.zip|.db|.csv|.h5|.bun|.stl|.log|.paramOpt"
+    + r"|*/.sdds"
 )
 
 _EXCLUDE_DIRS = re.compile(
