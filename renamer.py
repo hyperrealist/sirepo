e9ebfa8eaa97d3110be0e45cfdecfbe5fdb77b60
gurhar1133 <ghns1133@gmail.com>
2023-04-07 00:16:57 +0000

    Fix #5725: regex improve

--- renamer.py
+++ renamer.py
@@ -14,8 +14,8 @@ _EXCLUDE_FILES = re.compile(
     + r".*(_console\.py)|^venv/"
     + r"|^run/"
     + r"|__pycache__/ "
-    + r"|.git|.cache|node_modules|react/public"
-    + r"|^.*\.(sdds|bun|png|jpg|woff|eot|ttf|tif|gif|ico|h5m|zip|db|csv|h5|stl|log|paramOpt)$"
+    + r"|/.git|/.cache|node_modules|react/public"
+    + r"|^.*\.(sdds|bun|png|jpg|woff|eot|ttf|tif|gif|ico|h5m|zip|db|csv|h5|stl|log|paramOpt|woff2)$"
 )
 
 _EXCLUDE_DIRS = re.compile(
